78d7b4bae35be4af907f4935c01d754660ba405a
alissonandrade2020 <alisson2d@hotmail.com>
2021-09-03 18:43:32 -0300

    Atualização de rotas e finalizando o sistema

--- db/indra.sql
+++ db/indra.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03-Set-2021 às 21:16
+-- Tempo de geração: 03-Set-2021 às 23:42
 -- Versão do servidor: 10.4.19-MariaDB
 -- versão do PHP: 8.0.6
 
@@ -39,9 +39,8 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`id_cliente`, `nome_cliente`, `cpf_cnpj`, `data`) VALUES
-(1, 'Alberia Beatriz Germano de Oliveira Andrade', '3263423423', '2021-09-01 16:30:07'),
-(2, 'Alisson de Andrade Araujo', '3234237423', '2021-09-01 16:30:07'),
-(10, 'Teste', '3432432432', '2021-09-03 18:13:16');
+(1, 'Alberia Beatriz Germano', '3263423423', '2021-09-01 16:30:07'),
+(2, 'Alisson de Andrade Araujo', '3234237423', '2021-09-01 16:30:07');
 
 -- --------------------------------------------------------
 
@@ -61,8 +60,6 @@ CREATE TABLE `clientes_produtos` (
 --
 
 INSERT INTO `clientes_produtos` (`id_cliente_produto`, `id_cliente`, `id_produto`, `data`) VALUES
-(1, 1, 1, '2019-06-25 21:06:55'),
-(2, 1, 2, '2019-06-25 21:06:55'),
 (3, 2, 3, '2019-06-25 21:06:55');
 
 -- --------------------------------------------------------
@@ -84,9 +81,8 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id_produto`, `descricao`, `quantidade`, `preco`, `data`) VALUES
-(1, 'TV SMART', '28', 515.00, '2021-09-01 21:40:00'),
-(2, 'CELULAR 2', '35', 1.50, '2021-09-01 21:40:00'),
-(3, 'NOTEBOOK', '84', 1478.00, '2021-09-01 21:40:00');
+(2, 'CELULAR', '35', 144.00, '2021-09-01 21:40:00'),
+(3, 'NOTEBOOK', '84', 1470.00, '2021-09-01 21:40:00');
 
 -- --------------------------------------------------------
 
