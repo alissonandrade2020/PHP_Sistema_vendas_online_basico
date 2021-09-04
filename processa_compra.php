276b6113fbfb6a9a85f916d3188f956ad6ba275b
alissonandrade2020 <alisson2d@hotmail.com>
2021-09-04 11:02:25 -0300

    Atualização de rotas e finalizando o sistema

--- processa_compra.php
+++ processa_compra.php
@@ -5,7 +5,7 @@ include "model/db.php";
 $id_cliente = $_POST['escolha_cliente'];
 $id_produto = $_POST['escolha_produto'];
 
-$query = "INSERT INTO cliente_produto(id_cliente, id_produto) VALUES ('$id_cliente', '$id_produto')";
+$query = "INSERT INTO clientes_produtos(id_cliente, id_produto) VALUES ('$id_cliente', '$id_produto')";
 
 mysqli_query($conexao, $query);
 
